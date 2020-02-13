081b445d45b3110cff72d3d2ec7ca019103af4a4
Ant Iva <sg.antiiva@gmail.com>
2020-02-13 15:29:09 +0200

    display gui complete

--- todo-v10.js
+++ todo-v10.js
@@ -25,19 +25,18 @@ const todos = {
 
   toggleAll: function () {
     let isAllCompleted = false;
-    let counter = 0;
-    for (let item of this.list) {
-      if (item.completed) {
-        counter++;
-      }
-    }
+    const counter = this.list.reduce((acc, elem) => {
+      if (elem.completed) acc++;
+      return acc;
+    }, 0);
     if (counter === this.list.length) {
       isAllCompleted = true;
     }
-    for (let item of this.list) {
-      if (isAllCompleted) item.completed = false;
-      else item.completed = true;
-    }
+
+    this.list.forEach(elem => {
+      if (isAllCompleted) elem.completed = false;
+      else elem.completed = true;
+    });
   },
 
   deleteTodo: function (index) {
@@ -45,11 +44,7 @@ const todos = {
   }
 };
 
-//test
-todos.addTodo("Make hoework");
-todos.addTodo("Learn JS");
-todos.addTodo("Have a rest");
-todos.updateTodo("Make homework properly", 0);
+
 
 const handlers = {
   addTodo: function () {
@@ -61,15 +56,9 @@ const handlers = {
     }
     view.displayTodos();
   },
-  updateTodo: function () {
-    const PositionInput = document.querySelector("#changeTodoPositionInput");
-    const TextInput = document.querySelector("#changeTodoTextInput");
-    const position = +PositionInput.value;
-    const newText = TextInput.value;
-    if (position != false && newText != false) {
+  updateTodo: function (newText, position) {
+    if (newText != false) {
       todos.updateTodo(newText, position);
-      PositionInput.value = null;
-      TextInput.value = null;
     }
     view.displayTodos();
   },
@@ -86,10 +75,20 @@ const handlers = {
     }
     view.displayTodos();
   },
-  deleteTodo: function (event) {
-    const position = event.target.parentNode.id;
-    if (position != null) {
-      todos.deleteTodo(position);
+  handleTodo: function (event) {
+    if (event.target.className === "del-but") {
+      const position = +event.target.parentNode.id;
+      if (position != null) {
+        todos.deleteTodo(position);
+      }
+    } else if (event.target.className === "toggle") {
+      const position = +event.target.parentNode.id;
+      if (position != null) {
+        todos.toggleCompleted(position);
+      }
+    } else if (event.target.className === "display-text") {
+      event.target.edit();
+      console.log("display-text focused");
     }
     view.displayTodos();
   }
@@ -105,29 +104,56 @@ const view = {
       const liElem = document.createElement("li");
       liElem.className = "display-row";
       liElem.id = index;
-      let displayText = elem.todoText;
 
-      if (elem.completed) displayText = "(x)" + displayText;
-      else displayText = "( )" + displayText;
-      const textElem = this.createTextElem(displayText);
+      const checkBoxElem = document.createElement('input');
+      checkBoxElem.type = "checkbox";
+      checkBoxElem.className = "toggle";
+      if (elem.completed) {
+        checkBoxElem.setAttribute("checked", "");
+        liElem.classList.add("completed");
+      } else {
+        checkBoxElem.removeAttribute("checked");
+        liElem.classList.remove("completed");
+      }
+      
+
+      const displayText = elem.todoText;
+      const textElem = this.createTextElem(displayText, liElem.id, elem.completed);
       const delButElem = this.createDelButton();
 
+      liElem.appendChild(checkBoxElem);
       liElem.appendChild(textElem)
       liElem.appendChild(delButElem);
       ulElem.appendChild(liElem);
     });
   },
-  createTextElem: function (text) {
+  createTextElem: function (text, parentId, isCompleted) {
     const textElem = document.createElement("p");
     textElem.textContent = text;
-    textElem.className = "display-cell";
+    if (!isCompleted) {
+      textElem.setAttribute("contenteditable", "true");
+    } else textElem.removeAttribute("contenteditable");
+    
+    textElem.className = "display-text";
+    textElem.onclick = function(event) {
+      event.stopPropagation(); 
+    }
+    textElem.onblur = function(event) {
+      handlers.updateTodo(event.target.textContent, parentId);
+    }
     return textElem;
   },
   createDelButton: function () {
-    const delButElem = document.createElement("button");
-    delButElem.textContent = "Delete";
-    delButElem.className = "small";
-    delButElem.onclick = handlers.deleteTodo;
-    return delButElem;
+    const closeButElem = document.createElement("span");
+    closeButElem.textContent = "+";
+    closeButElem.className = "del-but";
+    return closeButElem;
   }
-}
+};
+
+//test
+todos.addTodo("Make hoework");
+todos.addTodo("Learn JS");
+todos.addTodo("Have a rest");
+todos.updateTodo("Make homework properly", 0);
+todos.toggleCompleted(1);
